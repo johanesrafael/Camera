1e2073c4996d74efbb1d58827332adfdc22f13c4
RDaneelOlivaw <duckfrost@gmail.com>
2019-02-15 00:58:05 +0100

    Added corrections

--- raspberry_pi_scripts/camera_tests/camera_setup.sh
+++ raspberry_pi_scripts/camera_tests/camera_setup.sh
@@ -3,7 +3,7 @@ sudo pip install picamera
 
 # Add in /boot/config.txt 
 #start_x=1
-#gpu_mem=128<span style="font-family: arial, helvetica, sans-serif; font-size: 14pt;"> </span>
+#gpu_mem=128
 
 sudo reboot
 
