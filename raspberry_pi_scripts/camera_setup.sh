9389b0239b36303444c755eb64cfef5e5eb57299
RDaneelOlivaw <duckfrost@gmail.com>
2019-02-15 00:43:41 +0100

    Added Camera code

---
+++ raspberry_pi_scripts/camera_setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get install libraspberrypi-dev
+sudo pip install picamera
+
+# Add in /boot/config.txt 
+#start_x=1
+#gpu_mem=128<span style="font-family: arial, helvetica, sans-serif; font-size: 14pt;"> </span>
+
+sudo reboot
+
