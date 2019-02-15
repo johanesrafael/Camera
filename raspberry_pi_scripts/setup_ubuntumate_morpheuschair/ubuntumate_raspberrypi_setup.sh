c6217112007e568a8d77608f26e4007d35d45c86
RDaneelOlivaw <duckfrost@gmail.com>
2019-02-15 01:44:43 +0100

    Added terminator install

--- raspberry_pi_scripts/setup_ubuntumate_morpheuschair/ubuntumate_raspberrypi_setup.sh
+++ raspberry_pi_scripts/setup_ubuntumate_morpheuschair/ubuntumate_raspberrypi_setup.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt install vim
 sudo apt install git
 sudo apt install openssh-server
+sudo apt install terminator
 sudo ufw allow 22
 sudo systemctl enable ssh
 sudo systemctl start ssh
