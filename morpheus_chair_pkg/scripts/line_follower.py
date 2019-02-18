6bc4329a44db57698ff01ea5a153991f73965f86
RDaneelOlivaw <duckfrost@gmail.com>
2019-02-18 19:13:34 +0100

    Correctimg bug

--- morpheus_chair_pkg/scripts/line_follower.py
+++ morpheus_chair_pkg/scripts/line_follower.py
@@ -14,100 +14,104 @@ class LineFollower(object):
         (self.major, minor, _) = cv2.__version__.split(".")
         rospy.logwarn("OpenCV Version Installed==>"+str(self.major))
 
+        # This way we process only half the frames
+        self.process_this_frame = True
+
         self.bridge_object = CvBridge()
         self.image_sub = rospy.Subscriber(camera_topic, Image, self.camera_callback)
         self.cmd_vel_pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size=1)
 
     def camera_callback(self, data):
 
-        try:
-            # We select bgr8 because its the OpenCV encoding by default
-            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
-        except CvBridgeError as e:
-            print(e)
-
-        # We get image dimensions and crop the parts of the image we dont need
-        # Bare in mind that because its image matrix first value is start and second value is down limit.
-        # Select the limits so that it gets the line not too close, not too far and the minimum portion possible
-        # To make process faster.
-        # TODO: Get multiple lines so that we can generate paths.
-
-        small_frame = cv2.resize(cv_image, (0, 0), fx=0.2, fy=0.2)
-
-        height, width, channels = small_frame.shape
-
-        rospy.loginfo("height=%s, width=%s" % (str(height), str(width)))
-
-        #descentre = 160
-        #rows_to_watch = 100
-        #crop_img = small_frame[(height) / 2 + descentre:(height) / 2 + (descentre + rows_to_watch)][1:width]
-        crop_img = small_frame
-
-        # Convert from RGB to HSV
-        hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-
-        lower_yellow = np.array([20, 100, 100])
-        upper_yellow = np.array([50, 255, 255])
-
-        # Threshold the HSV image to get only yellow colors
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-        # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(crop_img, crop_img, mask=mask)
-
-        if self.major == '3':
-            # If its 3
-            (_, contours, _) = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-
-        else:
-            # If its 2 or 4
-            (contours, _) = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-        rospy.loginfo("Number of centroids==>" + str(len(contours)))
-        centres = []
-        for i in range(len(contours)):
-            moments = cv2.moments(contours[i])
+        if self.process_this_frame:
             try:
-                centres.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
-                cv2.circle(res, centres[-1], 10, (0, 255, 0), -1)
-            except ZeroDivisionError:
-                pass
+                # We select bgr8 because its the OpenCV encoding by default
+                cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            except CvBridgeError as e:
+                print(e)
 
-        rospy.loginfo(str(centres))
-        # Select the right centroid
-        # [(542, 39), (136, 46)], (x, y)
-        most_right_centroid_index = 0
-        index = 0
-        max_x_value = 0
+            # We get image dimensions and crop the parts of the image we dont need
+            # Bare in mind that because its image matrix first value is start and second value is down limit.
+            # Select the limits so that it gets the line not too close, not too far and the minimum portion possible
+            # To make process faster.
+            # TODO: Get multiple lines so that we can generate paths.
 
-        centroids_detected = []
+            small_frame = cv2.resize(cv_image, (0, 0), fx=0.2, fy=0.2)
 
-        for candidate in centres:
-            # Retrieve the cx value
-            cx = candidate[0]
-            # Get the Cx more to the right
-            if cx >= max_x_value:
-                max_x_value = cx
-                most_right_centroid_index = index
-            index += 1
+            height, width, channels = small_frame.shape
 
-            try:
-                cx = centres[most_right_centroid_index][0]
-                cy = centres[most_right_centroid_index][1]
-                rospy.logwarn("Winner ==" + str(cx) + "," + str(cy) + "")
-            except:
-                cy, cx = height / 2, width / 2
+            rospy.loginfo("height=%s, width=%s" % (str(height), str(width)))
 
-            centroids_detected.append([cx,cy])
-            # Draw the centroid in the result image
-            cv2.circle(res, (int(cx), int(cy)), 5, (0, 0, 255), -1)
+            #descentre = 160
+            #rows_to_watch = 100
+            #crop_img = small_frame[(height) / 2 + descentre:(height) / 2 + (descentre + rows_to_watch)][1:width]
+            crop_img = small_frame
+
+            # Convert from RGB to HSV
+            hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+
+            lower_yellow = np.array([20, 100, 100])
+            upper_yellow = np.array([50, 255, 255])
+
+            # Threshold the HSV image to get only yellow colors
+            mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+            # Bitwise-AND mask and original image
+            res = cv2.bitwise_and(crop_img, crop_img, mask=mask)
+
+            if self.major == '3':
+                # If its 3
+                (_, contours, _) = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+
+            else:
+                # If its 2 or 4
+                (contours, _) = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+            rospy.loginfo("Number of centroids==>" + str(len(contours)))
+            centres = []
+            for i in range(len(contours)):
+                moments = cv2.moments(contours[i])
+                try:
+                    centres.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
+                    cv2.circle(res, centres[-1], 10, (0, 255, 0), -1)
+                except ZeroDivisionError:
+                    pass
+
+            rospy.loginfo(str(centres))
+            # Select the right centroid
+            # [(542, 39), (136, 46)], (x, y)
+            most_right_centroid_index = 0
+            index = 0
+            max_x_value = 0
+
+            centroids_detected = []
+
+            for candidate in centres:
+                # Retrieve the cx value
+                cx = candidate[0]
+                # Get the Cx more to the right
+                if cx >= max_x_value:
+                    max_x_value = cx
+                    most_right_centroid_index = index
+                index += 1
+
+                try:
+                    cx = centres[most_right_centroid_index][0]
+                    cy = centres[most_right_centroid_index][1]
+                    rospy.logwarn("Winner ==" + str(cx) + "," + str(cy) + "")
+                except:
+                    cy, cx = height / 2, width / 2
+
+                centroids_detected.append([cx,cy])
+                # Draw the centroid in the result image
+                cv2.circle(res, (int(cx), int(cy)), 5, (0, 0, 255), -1)
 
 
-        #cv2.imshow("Original", cv_image)
-        cv2.imshow("HSV", hsv)
-        cv2.imshow("MASK", mask)
-        #cv2.imshow("RES", res)
+            #cv2.imshow("Original", cv_image)
+            cv2.imshow("HSV", hsv)
+            cv2.imshow("MASK", mask)
+            #cv2.imshow("RES", res)
 
-        cv2.waitKey(1)
+            cv2.waitKey(0.1)
 
     def loop(self):
         rospy.spin()
