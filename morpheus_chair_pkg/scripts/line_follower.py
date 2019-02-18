0d0aa6055e85a5e321626298b401d9cf1c5a1d81
RDaneelOlivaw <duckfrost@gmail.com>
2019-02-18 20:39:41 +0100

    Added change

--- morpheus_chair_pkg/scripts/line_follower.py
+++ morpheus_chair_pkg/scripts/line_follower.py
@@ -105,7 +105,7 @@ class LineFollower(object):
                 try:
                     cx = centres[most_right_centroid_index][0]
                     cy = centres[most_right_centroid_index][1]
-                    rospy.logwarn("Winner ==" + str(cx) + "," + str(cy) + "")
+                    rospy.logwarn("Centroid FOUND ==" + str(cx) + "," + str(cy) + "")
                 except:
                     cy, cx = height / 2, width / 2
 
@@ -130,5 +130,5 @@ class LineFollower(object):
 if __name__ == '__main__':
     rospy.init_node('line_follower_start', anonymous=True)
     rgb_to_track = [77,32,49]
-    robot_mover = LineFollower(rgb_to_track=rgb_to_track, colour_error_perc= 50.0, colour_cal=False)
+    robot_mover = LineFollower(rgb_to_track=rgb_to_track, colour_error_perc= 20.0, colour_cal=False)
     robot_mover.loop()
