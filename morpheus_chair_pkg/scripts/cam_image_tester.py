7c651ae4eff58703b7ac51abbf0b9831dc9bce08
RDaneelOlivaw <duckfrost@gmail.com>
2019-03-07 14:53:16 +0100

    Added 5fps cam

--- morpheus_chair_pkg/scripts/cam_image_tester.py
+++ morpheus_chair_pkg/scripts/cam_image_tester.py
@@ -15,6 +15,7 @@ class CamTester(object):
         # This way we process only half the frames
         self.process_this_frame = True
         self.droped_frames = 0
+        self.process_image_ignore = False
 
         self.bridge_object = CvBridge()
         self.image_sub = rospy.Subscriber(camera_topic, Image, self.camera_callback)
@@ -24,6 +25,7 @@ class CamTester(object):
 
         # It seems that making tests, the rapsicam doesnt update the image until 6 frames have passed
         self.process_this_frame = self.droped_frames >= 6
+
 
         if self.process_this_frame:
             # We reset the counter
@@ -41,10 +43,14 @@ class CamTester(object):
             height, width, channels = small_frame.shape
 
             rospy.logdebug("height=%s, width=%s" % (str(height), str(width)))
-            cv2.imshow("Original", small_frame)
-            cv2.waitKey(1)
-            cv2.imshow("Original2", small_frame)
-            cv2.waitKey(1)
+
+            if self.process_image_ignore:
+                print("IGNORE FRAME...")
+                self.process_image_ignore = False
+            else:
+                cv2.imshow("Original", small_frame)
+                cv2.waitKey(1)
+                self.process_image_ignore = True
 
             raw_input("Press to process next image")
 
