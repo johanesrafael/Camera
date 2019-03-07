5e3ce90d551024f9d9220f00c2e01cf494258601
RDaneelOlivaw <duckfrost@gmail.com>
2019-03-07 13:45:55 +0100

    Added camera tester launch and script

---
+++ morpheus_chair_pkg/scripts/cam_image_tester.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+
+class CamTester(object):
+    def __init__(self, camera_topic="/morpheus_bot/raspicam_node/image_raw"):
+
+        # We check which OpenCV version is installed.
+        (self.major, minor, _) = cv2.__version__.split(".")
+        rospy.logwarn("OpenCV Version Installed==>"+str(self.major))
+
+        # This way we process only half the frames
+        self.process_this_frame = True
+
+        self.bridge_object = CvBridge()
+        self.image_sub = rospy.Subscriber(camera_topic, Image, self.camera_callback)
+
+
+    def camera_callback(self, data):
+
+        if self.process_this_frame:
+            self.process_this_frame = False
+            try:
+                # We select bgr8 because its the OpenCV encoding by default
+                cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            except CvBridgeError as e:
+                print(e)
+                cv_image = None
+
+            small_frame = cv2.resize(cv_image, (0, 0), fx=0.2, fy=0.2)
+
+            height, width, channels = small_frame.shape
+
+            rospy.logdebug("height=%s, width=%s" % (str(height), str(width)))
+            cv2.imshow("Original", small_frame)
+            cv2.waitKey(1)
+
+            raw_input("Press to process next image")
+
+        else:
+            self.process_this_frame = True
+            
+
+    def loop(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('cam_image_test_start', anonymous=True)
+    cam_tester = CamTester()
+    cam_tester.loop()
